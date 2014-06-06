c4a3acbc212478ce44a46eb388dd16cc83870b48
He Liu <liulonnie@gmail.com>
2014-06-06 14:36:46 -0700

    types

--- ir/types/types.go
+++ ir/types/types.go
@@ -1,18 +1,23 @@
 // Type representation
 package types
 
-type Type struct {
-	Name    string // type name
-	IsBasic bool   // if it is basic type
-	Size    uint32
-	Align   uint32
-
-	Fields   []*TypeField
-	FieldMap map[string]*TypeField
+type TypeBox struct {
+	Name  string // type name
+	Size  uint32
+	Align uint32
 }
 
-type TypeField struct {
-	Name   string
-	Offset uint32
-	Type   *Type
+type BasicType struct {
+	*TypeBox
 }
+
+func newBasic(name string, size uint32) *BasicType {
+	return &BasicType{&TypeBox{name, size, size}}
+}
+
+var (
+	Int32Type  = newBasic("int32", 4)
+	Uint32Type = newBasic("uint32", 4)
+	Int8Type   = newBasic("int8", 1)
+	Uint8Type  = newBasic("uint8", 1)
+)
