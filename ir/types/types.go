0d0bec2e5110bd587aed30ebfbbc73d5108f5b9c
He Liu <liulonnie@gmail.com>
2014-06-06 12:00:25 -0700

    .

--- ir/types/types.go
+++ ir/types/types.go
@@ -1,8 +1,11 @@
-package ir
+// Type representation
+package types
 
 type Type struct {
-	Size  uint32
-	Align uint32
+	Name    string // type name
+	IsBasic bool   // if it is basic type
+	Size    uint32
+	Align   uint32
 
 	Fields   []*TypeField
 	FieldMap map[string]*TypeField
